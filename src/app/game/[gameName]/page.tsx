ce4f72eba68ceb474b6486938773e8fab221dd18
abdoemadselim <abdoemadselim11@gmail.com>
2024-07-30 21:37:03 +0300

    Improve the performance

--- src/app/game/[gameName]/page.tsx
+++ src/app/game/[gameName]/page.tsx
@@ -1,8 +1,6 @@
 import SideBar from "@/components/sideBar";
 import { fetchGame } from "@/lib/games";
 import { Metadata } from "next";
-import Image from "next/image";
-import { Suspense } from "react";
 
 type Props = {
   params: {
@@ -24,17 +22,7 @@ export default async function Game({ params: { gameName } }: Props) {
 
   return (
     <div className="flex min-h-screen bg-primary">
-      <Suspense
-        fallback={
-          <Image
-            src="/images/loader.svg"
-            alt="loader"
-            width={200}
-            height={200}
-          />
-        }>
-        <SideBar hideText={true}/>
-      </Suspense>
+      <SideBar hideText={true} />
       <div className="flex flex-1 flex-col items-center justify-center w-full h-full bg-gradient-to-br from-[#FFC107] to-[#FF9800] p-8 md:p-6">
         <h2 className="text-4xl mb-5 font-bold text-right">{name}</h2>
         <iframe src={url} className="w-full h-[700px] d-block" width="100%" />
